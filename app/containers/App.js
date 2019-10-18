6ff15b28a5ab951533da9f9f49c4814ad579014a
John H <johnhayesru@gmail.com>
2019-10-18 14:07:57 +0300

    Initial commit.

---
+++ app/containers/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <header id="header" className="header-fs">
+      <h1 className="text-lg">Welcome to React</h1>
+    </header>
+  );
+}
+
+export default App;
---
+++ app/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./simple.css";
+import App from "./containers/App";
+
+const app = document.getElementById("app");
+ReactDOM.render(<App />, app);
