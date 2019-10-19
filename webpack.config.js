673f05e6439e1992d9addc3de1fc7c2176509413
John H <johnhayesru@gmail.com>
2019-10-20 01:05:06 +0300

    Added support for .env variables.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./app/index.js",
@@ -18,6 +19,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./app/index.html"
-    })
+    }),
+    new Dotenv()
   ]
 };
